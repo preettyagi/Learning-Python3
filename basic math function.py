d820b0a8ff0b245feddf4bae0e339976a8e1b79b
Preet Tyagi <43703200+preettyagi@users.noreply.github.com>
2020-04-23 22:01:13 +0530

    Basic maths function's
    Function's like addition, subtraction, multiplication, division.

---
+++ basic math function.py
@@ -0,0 +1,31 @@
+print("1. Add")
+print("2. Subtract")
+print("3. Multiplication")
+print("4. Divide")
+choice = int(input("Input your choice\n"))
+if choice == 1:
+    print(">>>>>>>>  Addition  <<<<<<<<<")
+    n1 = int(input("Enter a number: "))
+    n2 = int(input("Enter a Number: "))
+    sum = n1 + n2
+    print("Result: ", sum)
+elif choice == 2:
+    print(">>>>>>>>  Subtraction  <<<<<<<<<")
+    n1 = int(input("Enter a number: "))
+    n2 = int(input("Enter a Number: "))
+    minus = n1 - n2
+    print("Result: ", minus)
+elif choice == 3:
+    print(">>>>>>>>  Multiplication  <<<<<<<<<")
+    n1 = int(input("Enter a number: "))
+    n2 = int(input("Enter a Number: "))
+    multi = n1 * n2
+    print("Result: ", multi)
+elif choice == 4:
+    print(">>>>>>>>  Division  <<<<<<<<<")
+    n1 = int(input("Enter a number: "))
+    n2 = int(input("Enter a Number: "))
+    div = n1 + n2
+    print("Result: ", div)
+else:
+    print("!!!!!!!  Wrong Choice  !!!!!!!!!")
